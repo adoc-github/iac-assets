168b42bccaed079a6c1f26742848f1db1f2047ac
adoc-github <65012009+adoc-github@users.noreply.github.com>
2022-11-10 13:32:02 +0900

    Create IaC Assets repository.

---
+++ ec2-instance/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "testEC2" {
+    ami = var.ami
+    instance_type = var.instance_type
+    subnet_id = "subnet-0124294163b6e76e0"
+    tags = {
+        Name = "testEC2"
+    }
+}
---
+++ s3-webapp-brainboard/main.tf
@@ -0,0 +1,61 @@
+# Brainboard auto-generated file.
+
+resource "aws_s3_bucket" "bucket" {
+  force_destroy = true
+  bucket        = "${var.prefix}-${var.name}"
+
+  tags = {
+    env      = "Test"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  error_document {
+    key = "error.html"
+  }
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+  bucket = aws_s3_bucket.bucket.id
+}
+
+resource "aws_s3_bucket_object" "webapp" {
+  key          = "index.html"
+  content_type = "text/html"
+  content      = file("${path.module}/assets/index.html")
+  bucket       = aws_s3_bucket.bucket.id
+  acl          = "public-read"
+
+  tags = {
+    env      = "Test"
+  }
+}
